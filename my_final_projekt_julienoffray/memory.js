bb2397ca979ddd92d59007a836ace1b4277018f0
Julien1703 <julien.offray@gmail.com>
2023-06-05 17:57:14 +0200

    v2

--- my_final_projekt_julienoffray/memory.js
+++ my_final_projekt_julienoffray/memory.js
@@ -1,8 +1,24 @@
-// Array 20 Paare
-const karten = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P', 'Q', 'Q', 'R', 'R', 'S', 'S', 'T', 'T', 'U', 'U'];
+const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const kartenElemente = [];
+
+const anzahlKartenPaare = 4; // achtung: max länge von alphabet
+const anzahlColumns = 3;
+
+let currentPlayer = 1;
+let cardsPlayer1 = [];
+let cardsPlayer2 = [];
+
+let selectedCard1 = null;
+let selectedCard2 = null;
+
 
 // mischt karten
-function mischeArray(array) {
+function getKarten() {
+  let array = [];
+  for (let i = 0; i < anzahlKartenPaare; i++) {
+    array.push(alphabet[i]);
+    array.push(alphabet[i]);
+  }
   let currentIndex = array.length, temporaryValue, randomIndex;
 
   while (currentIndex !== 0) {
@@ -19,8 +35,8 @@ function mischeArray(array) {
 
 // erstellt Memory Raster 
 function erstelleKartenRaster() {
-  const kartenRaster = document.getElementById('karten-raster');
-  const gemischteKarten = mischeArray(karten);
+  const kartenRaster = document.getElementById('card-grid');
+  const gemischteKarten = getKarten();
 
   //befüllt das Memory Raster
   for (let i = 0; i < gemischteKarten.length; i++) {
@@ -28,41 +44,88 @@ function erstelleKartenRaster() {
     karte.classList.add('card', 'back');
     karte.dataset.wert = gemischteKarten[i];
     kartenRaster.appendChild(karte);
+    kartenElemente.push(karte);
   }
 }
 
-// Klick Event hinzufügen
-document.addEventListener('DOMContentLoaded', function () {
+// Initialisierung des Spiels
+window.onload = (event) => {
+
+  document.querySelector('#currentPlayer').textContent = currentPlayer;
+  document.querySelector('#card-grid').style.gridTemplateColumns = `repeat(${anzahlColumns},1fr)`;
+
   erstelleKartenRaster();
 
-  const karten = document.querySelectorAll('.card');
-  let vorherigeKarte = null;
-
-  karten.forEach(function (karte) {
+  kartenElemente.forEach(function (karte) {
     karte.addEventListener('click', function () {
       if (this.classList.contains('back')) {
-        this.classList.remove('back');
-        const wert = this.dataset.wert;
-        this.textContent = wert;
-
-        if (!vorherigeKarte) {
-          vorherigeKarte = this;
-        } else {
-          if (vorherigeKarte.dataset.wert === this.dataset.wert) {
-            vorherigeKarte.classList.add('matched');
-            this.classList.add('matched');
-            vorherigeKarte = null;
-          } else {
-            setTimeout(() => {
-              vorherigeKarte.classList.add('back');
-              this.classList.add('back');
-              vorherigeKarte.textContent = '';
-              this.textContent = '';
-              vorherigeKarte = null;
-            }, 1000);
-          }
+        if (!selectedCard1) {
+          selectedCard1 = this;
+          turnCard(selectedCard1);
+        }
+        else if (!selectedCard2) {
+          selectedCard2 = this;
+          turnCard(selectedCard2);
+          checkWin();
         }
       }
     });
   });
-});
+}
+
+function turnCard(card) {
+  card.classList.remove('back');
+  const wert = card.dataset.wert;
+  card.textContent = wert;
+}
+
+function checkWin() {
+  if (selectedCard1.dataset.wert === selectedCard2.dataset.wert) {
+    // wenn match
+    selectedCard1.classList.add('matched');
+    selectedCard2.classList.add('matched');
+    if (currentPlayer === 1) {
+      cardsPlayer1.push(selectedCard1.dataset.wert);
+      document.querySelector('#cardsPlayer1').innerHTML = cardsPlayer1.join(", ");
+    }
+    else {
+      cardsPlayer2.push(selectedCard1.dataset.wert);
+      document.querySelector('#cardsPlayer2').innerHTML = cardsPlayer2.join(", ");
+    }
+    setTimeout(() => {
+      selectedCard1.classList.add('invisible');
+      selectedCard2.classList.add('invisible');
+      selectedCard1.classList.remove('match');
+      selectedCard2.classList.remove('match');
+      selectedCard1 = null;
+      selectedCard2 = null;
+    }, 1000);
+
+  } else {
+    // kein match
+    selectedCard1.classList.add('nomatch');
+    selectedCard2.classList.add('nomatch');
+    setTimeout(() => {
+      selectedCard1.classList.add('back');
+      selectedCard2.classList.add('back');
+      selectedCard1.classList.remove('nomatch');
+      selectedCard2.classList.remove('nomatch');
+      selectedCard1.textContent = '';
+      selectedCard2.textContent = '';
+      selectedCard1 = null;
+      selectedCard2 = null;
+      switchPlayer();
+    }, 1000);
+  }
+}
+
+function switchPlayer() {
+  if (currentPlayer === 1) {
+    currentPlayer = 2;
+  }
+  else {
+    currentPlayer = 1;
+  }
+  document.querySelector('#currentPlayer').textContent = currentPlayer;
+}
+
