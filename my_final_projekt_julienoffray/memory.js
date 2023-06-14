1df958c529dd08c4068b67354727dc907ef7f8b1
Julien1703 <julien.offray@gmail.com>
2023-06-14 21:24:43 +0200

    fixbeimatch

--- my_final_projekt_julienoffray/memory.js
+++ my_final_projekt_julienoffray/memory.js
@@ -25,22 +25,72 @@ let timerSeconds = 0;
 let anzahlKartenPaare = 4;
 let anzahlColumns = 2;
 
+window.onload = (event) => {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      switch (button.textContent) {
+        case '15 Paare':
+          anzahlKartenPaare = 5;
+          anzahlColumns = 5;
+          break;
+        case '20 Paare':
+          anzahlKartenPaare = 20;
+          anzahlColumns = 6;
+          break;
+        case '25 Paare':
+          anzahlKartenPaare = 25;
+          anzahlColumns = 7;
+          break;
+        case '35 Paare':
+          anzahlKartenPaare = 35;
+          anzahlColumns = 8;
+          break;
+        default:
+          anzahlKartenPaare = 4;
+          anzahlColumns = 2;
+      }
+
+      startGame();
+
+      document.querySelector('#currentPlayer').textContent = currentPlayer;
+      document.querySelector('#timer').textContent = getFormattedTime(timerSeconds);
+      document.querySelector('#card-grid').style.gridTemplateColumns = `repeat(${anzahlColumns},1fr)`;
+
+      kartenElemente.forEach(function (karte) {
+        karte.addEventListener('click', function () {
+          if (this.classList.contains('back')) {
+            if (!selectedCard1) {
+              selectedCard1 = this;
+              turnCard(selectedCard1);
+            } else if (!selectedCard2) {
+              selectedCard2 = this;
+              turnCard(selectedCard2);
+              checkWin();
+            }
+          }
+        });
+      });
+    });
+  });
+};
+
 function getKarten() {
   let array = [];
   for (let i = 0; i < anzahlKartenPaare; i++) {
-      array.push(alphabet[i]);
-      array.push(alphabet[i]);
+    array.push(alphabet[i]);
+    array.push(alphabet[i]);
   }
 
   let currentIndex = array.length;
 
   while (currentIndex !== 0) {
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-      let temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+    let temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
   return array;
 }
@@ -50,25 +100,24 @@ function erstelleKartenRaster() {
   const gemischteKarten = getKarten();
 
   while (kartenRaster.firstChild) {
-      kartenRaster.removeChild(kartenRaster.firstChild);
+    kartenRaster.removeChild(kartenRaster.firstChild);
   }
 
   kartenElemente.length = 0;
 
   for (let i = 0; i < gemischteKarten.length; i++) {
-      const karte = document.createElement('div');
-      karte.classList.add('card', 'back');
-      karte.dataset.wert = gemischteKarten[i];  // Jetzt ist das der Pfad zum Bild
-      kartenRaster.appendChild(karte);
-      kartenElemente.push(karte);
+    const karte = document.createElement('div');
+    karte.classList.add('card', 'back');
+    karte.dataset.wert = gemischteKarten[i];
+    kartenRaster.appendChild(karte);
+    kartenElemente.push(karte);
   }
-
 }
 
 function startTimer() {
   timer = setInterval(() => {
-      timerSeconds++;
-      document.querySelector('#timer').textContent = getFormattedTime(timerSeconds);
+    timerSeconds++;
+    document.querySelector('#timer').textContent = getFormattedTime(timerSeconds);
   }, 1000);
 }
 
@@ -89,124 +138,79 @@ function getFormattedTime(seconds) {
   return `${minutes}:${remainingSeconds}`;
 }
 
-window.onload = (event) => {
-  const buttons = document.querySelectorAll('button');
-  buttons.forEach(function (button) {
-      button.addEventListener('click', function () {
-          switch (button.textContent) {
-              case '15 Paare':
-                  anzahlKartenPaare = 2;
-                  anzahlColumns = 5;
-                  break;
-              case '20 Paare':
-                  anzahlKartenPaare = 20;
-                  anzahlColumns = 6;
-                  break;
-              case '25 Paare':
-                  anzahlKartenPaare = 25;
-                  anzahlColumns = 7;
-                  break;
-              case '35 Paare':
-                  anzahlKartenPaare = 35;
-                  anzahlColumns = 8;
-                  break;
-              default:
-                  anzahlKartenPaare = 4;
-                  anzahlColumns = 2;
-          }
-
-          startGame();
-
-          document.querySelector('#currentPlayer').textContent = currentPlayer;
-          document.querySelector('#timer').textContent = getFormattedTime(timerSeconds);
-          document.querySelector('#card-grid').style.gridTemplateColumns = `repeat(${anzahlColumns},1fr)`;
-
-          kartenElemente.forEach(function (karte) {
-              karte.addEventListener('click', function () {
-                  if (this.classList.contains('back')) {
-                      if (!selectedCard1) {
-                          selectedCard1 = this;
-                          turnCard(selectedCard1);
-                      } else if (!selectedCard2) {
-                          selectedCard2 = this;
-                          turnCard(selectedCard2);
-                          checkWin();
-                      }
-                  }
-              });
-          });
-      });
-  });
-}
-
 function turnCard(card) {
   const wert = card.dataset.wert;
   card.style.backgroundImage = `url(${wert})`;
   card.classList.remove('back');
-  //   const wert = card.dataset.wert;//
-  //   card.textContent = wert;
-
   card.classList.add('flip');
   card.addEventListener('animationend', function () {
-      card.classList.remove('flip');
+    card.classList.remove('flip');
   });
 }
 
 function checkWin() {
   if (selectedCard1.dataset.wert === selectedCard2.dataset.wert) {
-      selectedCard1.classList.add('matched');
-      selectedCard2.classList.add('matched');
+    // Karten matchen
+    selectedCard1.classList.add('matched');
+    selectedCard2.classList.add('matched');
+    setTimeout(() => {
+      // Karten nach einer Verzögerung ausblenden
+      selectedCard1.style.opacity = '0';
+      selectedCard2.style.opacity = '0';
+      selectedCard1.style.pointerEvents = 'none';
+      selectedCard2.style.pointerEvents = 'none';
       if (currentPlayer === 1) {
-          cardsPlayer1.push(selectedCard1.dataset.wert);
-          document.querySelector('#cardsPlayer1').innerHTML = cardsPlayer1.join(", ");
+        cardsPlayer1.push(selectedCard1.dataset.wert);
+        document.querySelector('#cardsPlayer1').innerHTML = cardsPlayer1.join(", ");
       } else {
-          cardsPlayer2.push(selectedCard1.dataset.wert);
-          document.querySelector('#cardsPlayer2').innerHTML = cardsPlayer2.join(", ");
+        cardsPlayer2.push(selectedCard1.dataset.wert);
+        document.querySelector('#cardsPlayer2').innerHTML = cardsPlayer2.join(", ");
       }
       selectedCard1 = null;
       selectedCard2 = null;
       switchPlayer();
+      checkGameEnd();
+    }, 1000); // Verzögerung von 1000 ms (1 Sekunde)
   } else {
-      setTimeout(() => {
-          selectedCard1.style.backgroundImage = '';
-          selectedCard1.classList.add('back');
-          selectedCard2.style.backgroundImage = '';
-          selectedCard2.classList.add('back');
-          selectedCard1.classList.remove('nomatch');
-          selectedCard2.classList.remove('nomatch');
-          selectedCard1 = null;
-          selectedCard2 = null;
-          switchPlayer();
-      }, 1000);
+    // Karten matchen nicht
+    setTimeout(() => {
+      // Karten nach einer Verzögerung wieder umdrehen
+      selectedCard1.style.backgroundImage = '';
+      selectedCard1.classList.add('back');
+      selectedCard2.style.backgroundImage = '';
+      selectedCard2.classList.add('back');
+      selectedCard1.classList.remove('nomatch');
+      selectedCard2.classList.remove('nomatch');
+      selectedCard1 = null;
+      selectedCard2 = null;
+      switchPlayer();
+      checkGameEnd();
+    }, 1000); // Verzögerung von 1000 ms (1 Sekunde)
   }
-  checkGameEnd();
 }
-
 
 function switchPlayer() {
   if (currentPlayer === 1) {
-      currentPlayer = 2;
+    currentPlayer = 2;
   } else {
-      currentPlayer = 1;
+    currentPlayer = 1;
   }
   document.querySelector('#currentPlayer').textContent = currentPlayer;
 }
 
 function checkGameEnd() {
   if (cardsPlayer1.length + cardsPlayer2.length === anzahlKartenPaare * 2) {
-      clearInterval(timer);
-      let winner = '';
-      if (cardsPlayer1.length > cardsPlayer2.length) {
-          winner = 'Spieler 1';
-      } else if (cardsPlayer2.length > cardsPlayer1.length) {
-          winner = 'Spieler 2';
-      } else {
-          winner = 'Unentschieden';
-      }
-      const spielzeit = getFormattedTime(timerSeconds);
-      alert(`Spiel beendet! Gewinner: ${winner}. In einer Zeit von ${spielzeit}`);
-      gameEnded = true;
+    clearInterval(timer);
+    let winner = '';
+    if (cardsPlayer1.length > cardsPlayer2.length) {
+      winner = 'Spieler 1';
+    } else if (cardsPlayer2.length > cardsPlayer1.length) {
+      winner = 'Spieler 2';
+    } else {
+      winner = 'Unentschieden';
+    }
+    const spielzeit = getFormattedTime(timerSeconds);
+    alert(`Spiel beendet! Gewinner: ${winner}. In einer Zeit von ${spielzeit}`);
+    gameEnded = true;
   }
-
-
 }
